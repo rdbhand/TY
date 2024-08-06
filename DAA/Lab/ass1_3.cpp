d106791b2ef36b879da9ab1ef43de817ebe6c420
Rameshwar Bhand <rdbhand308@gmail.com>
2024-08-06 08:57:49 +0530

    first commit

--- DAA/Lab/ass1_3.cpp
+++ DAA/Lab/ass1_3.cpp
@@ -5,21 +5,3 @@ Statement: Given a number n. The task is to find the smallest number whose facto
 Input : n = 1 Output : 5 
 Input : n = 6 Output : 25   CO1
 */
-
-#include<iostream>
-using namespace std;
-long long fact(long long x){
-  if(x==1 || x==0){
-    return 1;
-  }
-  return x*fact(x-1);
-}
-
-int main(){
-  long long res=fact(16);
-  for(int i=1;i<=20;i++){
-    res=fact(i);
-    cout<<i<<"==>"<<res<<endl;
-  }
-  return 0;
-}
