dea7f5a61b7854feb71d2012d50ded2dedac1a93
Rameshwar Bhand <rdbhand308@gmail.com>
2024-08-08 19:36:22 +0530

    8aug24

--- DAA/Lab/ass1_3.cpp
+++ DAA/Lab/ass1_3.cpp
@@ -5,3 +5,43 @@ Statement: Given a number n. The task is to find the smallest number whose facto
 Input : n = 1 Output : 5 
 Input : n = 6 Output : 25   CO1
 */
+
+#include <iostream>
+using namespace std;
+
+int cnt_trailing_zeroes(int x){
+  int cnt = 0;
+  while (x >= 5) {
+      x /= 5;
+      cnt += x;
+  }
+  return cnt;
+
+}
+
+int smallest_number(int n)
+{
+  int l = 0, r = 5 * n;
+  while (l < r)
+  {
+    int mid = (l + r) / 2;
+    if (cnt_trailing_zeroes(mid) < n)
+    {
+      l = mid + 1;
+    }
+    else
+    {
+      r = mid;
+    }
+  }
+  return l;
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  int res = smallest_number(n);
+  cout<<"Smallest Number with "<<n<<" trailing zeroes : "<<res<<endl;
+  return 0;
+}               
