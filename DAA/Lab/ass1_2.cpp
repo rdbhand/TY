30f974ab58948afec55ee9699c1cc641c4cacd78
Rameshwar Bhand <158077944+rdbhand@users.noreply.github.com>
2024-08-09 13:51:17 +0530

    Add files via upload

--- DAA/Lab/ass1_2.cpp
+++ DAA/Lab/ass1_2.cpp
@@ -1,5 +1,6 @@
 /*
-Implement a problem of move all zeroes to end of 
+
+2. Implement a problem of move all zeroes to end of 
 array.
 Statement: Given an array of random numbers, Push all the zero’s 
 of a given array to the end of the array. For example, if the given 
@@ -15,30 +16,87 @@ CO1
 #include<iostream>
 using namespace std;
 
-void push_zeroes_to_end(int *arr,int n){
-  int cnt=0;
-  for(int i=0;i<n;i++){
-    if(arr[i]!=0){
-      arr[cnt]=arr[i];
-      cnt++;
+void merge(int *arr, int l, int r)
+{
+  int mid = (l + r) / 2;
+  int len1 = mid - l + 1;
+  int len2 = r - mid;
+  int *left_arr = new int[len1];
+  int *right_arr = new int[len2];
+  int main_arr_ind = l;
+  for (int i = 0; i < len1; i++)
+  {
+    left_arr[i] = arr[main_arr_ind++];
+  }
+  main_arr_ind = mid + 1;
+  for (int i = 0; i < len2; i++)
+  {
+    right_arr[i] = arr[main_arr_ind++];
+  }
+
+  int ind1 = 0, ind2 = 0;
+  main_arr_ind = l;
+  while (ind1 < len1 && ind2 < len2)
+  {
+    if (left_arr[ind1] ==0 &&  right_arr[ind2]!=0)
+    {
+      arr[main_arr_ind++] = right_arr[ind2++];
+      ind1++;
+    }
+    else if(left_arr[ind1]!=0 && right_arr[ind2]==0)
+    {
+      arr[main_arr_ind++] = left_arr[ind1++];
+      ind2++;
     }
   }
-  while(cnt<n){
-    arr[cnt]=0;
-    cnt++;
+  while (ind1 < len1)
+  {
+  	if(left_arr[ind1]!=0){
+  		arr[main_arr_ind++] = left_arr[ind1++];
+  	}
+  }
+  while (ind2 < len2)
+  {
+  	if(right_arr[ind2]!=0){
+  		arr[main_arr_ind++] = right_arr[ind2++];
+  	}
+  }
+  while(main_arr_ind<(len1+len2)){
+  	arr[main_arr_ind++]=0;
   }
 }
+
+void merge_sort(int *arr, int l, int r)
+{
+  if (l >= r)
+  {
+    return;
+  }
+  int mid = (l + r) / 2;  
+  merge_sort(arr, l, mid);
+  merge_sort(arr, mid + 1, r);
+  merge(arr, l, r);
+}
+
+void move_zeroes(int *arr,int n){
+	int l=0,r=n-1;
+	merge_sort(arr,l,r);
+}
+
 int main(){
-  int n;
-  cin>>n;
-  int arr[n];
-  for(int i=0;i<n;i++){
-    cin>>arr[i];
-  }
-  push_zeroes_to_end(arr,n);
-  for(int i=0;i<n;i++){
-    cout<<arr[i]<<" ";
-  }
-  cout<<endl;
-  return 0;
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	
+	move_zeroes(arr,n);
+	
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+	
+	return 0;
 }
