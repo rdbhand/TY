bd62c32cb604c3170e6759c1675a6aa3be814e9e
Rameshwar Bhand <158077944+rdbhand@users.noreply.github.com>
2024-09-24 01:51:48 -0700

    24-09-24

---
+++ WT/Lab/ass4/bootstap/script.js
@@ -0,0 +1,30 @@
+function validate(){
+    var name=document.getElementById("uname").value;
+    if(name==""){
+      return alert('Enter username ! ');
+    }
+    name=document.getElementById("pass").value;
+    if(name==""){
+      return alert('Enter password ! ');
+    }
+    name=document.getElementById("email").value;
+    if(name==""){
+      return alert('Enter Email ! ');
+    }
+    name=document.getElementById("dob").value;
+    if(name==""){
+      return alert('Enter DOB !');
+    }
+    name=document.getElementById("age").value;
+    if(name==""){
+      return alert('Enter Age ! ');
+    }
+    name=document.getElementById("address").value;
+    if(name==""){
+      return alert('Enter Address ! ');
+    }
+    name=document.getElementById("pic").value;
+    if(name==""){
+      return alert('Attach file ! ');
+    }
+  }
